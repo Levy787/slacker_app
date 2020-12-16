2a4134e1fa2edeaeb31bce3005b59f679694824b
Levi <67333483+Levy787@users.noreply.github.com>
2020-12-16 08:56:49 -0800

    More Guide select work, going to refactor now

--- lib/screens/guide_select_screen.dart
+++ lib/screens/guide_select_screen.dart
@@ -1,13 +1,6 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:slacker/classes/guide_select_helper.dart';
-//import 'package:flutter/services.dart';
 import 'package:slacker/classes/highline_db_provider.dart';
-import 'package:slacker/constants.dart';
-import 'package:slacker/screens/state_detail_screen.dart';
-import 'package:slacker/widgets/state_tile.dart';
-import 'package:slacker/widgets/state_page.dart';
 
 class GuideSelectScreen extends StatefulWidget {
   static const id = 'guide_select_screen';
@@ -25,7 +18,7 @@ class _GuideSelectScreen extends State<GuideSelectScreen> {
   @override
   void initState() {
     super.initState();
-    widget.guideSelectHelper.createStatePages(widget.statesJSON);
+    //widget.guideSelectHelper.createStatePages(widget.statesJSON);
   }
 
   @override
@@ -64,88 +57,3 @@ class _GuideSelectScreen extends State<GuideSelectScreen> {
     );
   }
 }
-
-List<String> states = [
-  'Tasmania',
-  'New South Wales',
-  'Queensland',
-  'Victoria',
-  'Australian Capital Territory',
-  'Western Australia',
-  'South Australia',
-  'Northern Territory',
-];
-
-List<String> stateDetails = [
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-  'item 3',
-  'item 1',
-  'item 2',
-];
--- lib/widgets/state_page.dart
+++ lib/widgets/state_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:slacker/widgets/pill_dropdown.dart';
 import 'package:slacker/widgets/state_display_card.dart';
+import 'package:slacker/widgets/custom_expansion_tile.dart';
 
 //TODO: maybe instead of dots have all state names and the selected underlined or dropdown
 
@@ -47,7 +47,9 @@ class _StatePageState extends State<StatePage> {
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
+      cacheExtent: 2000, //TODO: Implement keep cache alive while in pageView
       slivers: [
+        ///This is the Image appBar
         SliverAppBar(
           flexibleSpace: Padding(
             padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
@@ -59,14 +61,17 @@ class _StatePageState extends State<StatePage> {
           pinned: false,
           automaticallyImplyLeading: false,
         ),
+
+        ///This is the Pinned Grey appBar
         SliverAppBar(
           automaticallyImplyLeading: false,
           flexibleSpace: Align(
             alignment: Alignment.topCenter,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                SizedBox(height: 5.0),
+                SizedBox(height: 7.5),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: generatePageIndexDots(
@@ -80,39 +85,44 @@ class _StatePageState extends State<StatePage> {
                       child: Text(
                         widget.stateName,
                         style: TextStyle(
-                          color: Colors.white,
+                          color: Colors.black,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
                   ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Line Type:'),
-                    SizedBox(width: 10.0),
-                    DropdownButton(
-                      value: 0,
-                      items: [
-                        DropdownMenuItem(child: Text('All'), value: 0),
-                        DropdownMenuItem(child: Text('Highline'), value: 1),
-                        DropdownMenuItem(child: Text('Midline'), value: 2),
-                        DropdownMenuItem(child: Text('Waterline'), value: 3),
-                        DropdownMenuItem(child: Text('Parkline'), value: 4),
-                      ],
-                      onChanged: (value) {
-                        print(value);
-                      },
-                    ),
-                  ],
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Line Type:',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      SizedBox(width: 10.0),
+                      DropdownButton(
+                        value: 0,
+                        items: [
+                          DropdownMenuItem(child: Text('All'), value: 0),
+                          DropdownMenuItem(child: Text('Highline'), value: 1),
+                          DropdownMenuItem(child: Text('Midline'), value: 2),
+                          DropdownMenuItem(child: Text('Waterline'), value: 3),
+                          DropdownMenuItem(child: Text('Parkline'), value: 4),
+                        ],
+                        onChanged: (value) {
+                          print(value);
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
           ),
           expandedHeight: 120,
           collapsedHeight: 120,
-          backgroundColor: Colors.grey[800],
+          backgroundColor: Colors.grey[300],
           elevation: 0,
           pinned: true,
           shape: RoundedRectangleBorder(
@@ -140,7 +150,6 @@ class _StatePageState extends State<StatePage> {
                 : generateRegionExpansionTiles(
                         widget.stateData, widget.stateName)
                     .length,
-            //childCount: widget.stateDetails.length,
           ),
         ),
       ],
@@ -152,14 +161,50 @@ List<Widget> generateRegionExpansionTiles(
     Map<String, Map<String, List<String>>> stateData, String stateName) {
   List<Widget> returnList = [];
   stateData.forEach((region, value) {
-    returnList.add(Theme(
-      data: ThemeData(
-        accentColor: Colors.black,
+    returnList.add(CustomExpansionTile(
+      headerUnselected: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(7.5), color: Colors.grey),
+        margin: EdgeInsets.all(5.0),
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 7.5),
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              region,
+              style: TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.w600,
+                color: Color(0xff13525b),
+              ),
+            ),
+            Icon(Icons.add, color: Color(0xff13525b)),
+          ],
+        ),
       ),
-      child: ExpansionTile(
-        title: Text(region),
-        children: generateAreaExpansionTiles(stateData[region]),
+      headerSelected: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(7.5), color: Color(0xff13525b)),
+        margin: EdgeInsets.all(5.0),
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 7.5),
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              region,
+              style: TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+            ),
+            Icon(Icons.remove, color: Colors.white),
+          ],
+        ),
       ),
+      children: generateAreaExpansionTiles(stateData[region]),
     ));
   });
   return returnList;
@@ -168,8 +213,9 @@ List<Widget> generateRegionExpansionTiles(
 List<Widget> generateAreaExpansionTiles(Map<String, List<String>> regionData) {
   List<Widget> returnList = [];
   regionData.forEach((area, value) {
-    returnList.add(ExpansionTile(
-      title: Text(area),
+    returnList.add(CustomExpansionTile(
+      headerUnselected: Text(area),
+      headerSelected: Text(area),
       children: generateGuideCards(regionData[area]),
     ));
   });
